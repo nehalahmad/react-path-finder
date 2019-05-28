1553f23cfd188d0f5d2234a7025c18fa448f7493
nehalahmad <nehal.ahmad@nagarro.com>
2019-05-28 13:56:27 +0530

    Modal logic enhanced

--- src/components/App.js
+++ src/components/App.js
@@ -109,6 +109,7 @@ export default class App extends Component {
    * @description: Message handler to set error message
    */
   _setErrorMessageHandler = errorMessage => {
+    document.body.classList.remove("modal-open");
     this.setState({ errorMessage });
   };
 
