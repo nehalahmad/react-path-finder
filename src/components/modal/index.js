c535700b54d2b8d88ae2ee82c22546e3feecc260
nehalahmad <nehal.ahmad@nagarro.com>
2019-05-28 18:02:42 +0530

    close modal clicking outside

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,42 +1,50 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 
-export default props => {
-  if (!props.errorMessage) {
-    return null;
-  }
+export default class Modal extends Component {
+  render() {
+    if (!this.props.errorMessage) {
+      return null;
+    }
 
-  return (
-    <Fragment>
-      <div
-        className="modal fade show"
-        tabIndex="-1"
-        role="dialog"
-        onClick={() => props.setErrorMessageHandler("")}
-      >
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-                onClick={() => props.setErrorMessageHandler("")}
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-              <h4 className="modal-title">Error</h4>
-            </div>
-            <div className="modal-body">
-              <p>{props.errorMessage}</p>
+    this._clickOutsideHandler = e => {
+      if (e.target.className && e.target.className.includes("show")) {
+        this.props.setErrorMessageHandler("");
+      }
+    };
+
+    return (
+      <Fragment>
+        <div
+          className="modal fade show"
+          tabIndex="-1"
+          role="dialog"
+          onClick={this._clickOutsideHandler}
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                  onClick={() => this.props.setErrorMessageHandler("")}
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 className="modal-title">Error</h4>
+              </div>
+              <div className="modal-body">
+                <p>{this.props.errorMessage}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div
-        className="modal-backdrop fade show"
-        onClick={() => props.setErrorMessageHandler("")}
-      />
-    </Fragment>
-  );
-};
+        <div
+          className="modal-backdrop fade show"
+          onClick={this._clickOutsideHandler}
+        />
+      </Fragment>
+    );
+  }
+}
