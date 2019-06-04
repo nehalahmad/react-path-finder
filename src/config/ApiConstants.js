082f6d2e03bf1c230dfdc23dfe464644f70fc1cc
nehalahmad <nehal.ahmad@nagarro.com>
2019-06-04 15:18:33 +0530

    file with uppercase name

---
+++ src/config/ApiConstants.js
@@ -0,0 +1,10 @@
+/**
+ * API status
+ */
+export const SUCCESS = "success";
+export const IN_PROGRESS = "in progress";
+export const FAIL = "failure";
+/**
+ * some other constants
+ */
+export const NUMBER_ATTEMPTS = 3;
---
+++ src/config/AppConstants.js
@@ -0,0 +1,19 @@
+export const SUBMIT_TEXT = "Submit";
+export const RESUBMIT_TEXT = "Re-Submit";
+export const RESET_TEXT = "Reset";
+export const DIRECTION_NOT_FOUND = "Direction couldn't found!";
+export const SOMETHING_WRONG = "Oops, something went wrong!";
+export const LOCATION = {
+  ONE: {
+    TITLE: "Starting location",
+    PLACEHOLDER: "enter starting location"
+  },
+  TWO: {
+    TITLE: "Drop-off point",
+    PLACEHOLDER: "enter drop-off point"
+  }
+};
+export const ERROR_DIRECTION_SERVICE = "Error in direction service response";
+export const UNAUTHORISED_LOCATIONS = "Locations couldn't authorized!";
+export const TOTAL_DISTANCE = "Total distance:";
+export const TOTAL_TIME = "Total time:";
